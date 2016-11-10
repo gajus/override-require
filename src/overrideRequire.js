bb58eca89f81026e0b75274f8159b9126e70987e
Gajus Kuizinas <gajus@gajus.com>
2016-11-10 23:09:12 +0000

    chore: use flow

--- src/overrideRequire.js
+++ src/overrideRequire.js
@@ -2,15 +2,31 @@
 
 import Module from 'module';
 
+type ParentType = {
+  children: Array<any>,
+  exports: any,
+  filename: string,
+  id: string,
+  loaded: boolean,
+  parent: any,
+  paths: Array<string>
+};
+
+type IsOverrideType = (request: string, parent: ParentType) => boolean;
+
+type ResolverOverrideType = (request: string, parent: ParentType) => any;
+
+type OverrideRequireType = (...rest: Array<void>) => void;
+
 /**
  * @param isOverride A condition used to check whether to override Module._load.
  * @param resolveOverride A function used to override Module._load result.
  */
-export default (isOverride: Function, resolveOverride: Function): Function => {
+export default (isOverride: IsOverrideType, resolveOverride: ResolverOverrideType): OverrideRequireType => {
   const originalLoad = Module._load;
 
   // eslint-disable-next-line id-match
-  Module._load = function (request, parent) {
+  Module._load = function (request: string, parent: ParentType) {
     if (isOverride(request, parent)) {
       return resolveOverride(request, parent);
     }
